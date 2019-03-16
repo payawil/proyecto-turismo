664261800174de198f63109c9251a6c79e3affe9
wilson quispe suri <payawil@gmail.con>
2019-03-16 08:15:01 -0400

    adicion de componentes de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { PieComponent } from './componentes/pie/pie.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    PieComponent
+    PieComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
